ba3a5ef0e35c2b35586a4d4538e5b04ff98dccc6
Daisuke Kobayashi <dreamers1632@gmail.com>
2015-12-20 00:01:07 +0900

    FirstCommit

---
+++ DiffBetweenObservableCollectionAndReactiveCollection/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DiffBetweenObservableCollectionAndReactiveCollection
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainWindowViewModel vm;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            vm = new MainWindowViewModel();
+            this.DataContext = vm;
+        }
+
+        private void ObservableCollectionButton_Click(object sender, RoutedEventArgs e)
+        {
+            vm.AddObservableCollection();
+        }
+
+        private void ReactiveCollectionButton_Click(object sender, RoutedEventArgs e)
+        {
+            vm.AddReactiveCollection();
+        }
+    }
+}
---
+++ DiffBetweenObservableCollectionAndReactiveCollection/MainWindowViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using Reactive.Bindings;
+
+namespace DiffBetweenObservableCollectionAndReactiveCollection
+{
+    class MainWindowViewModel
+    {
+        public ReactiveCollection<string> reactiveCollection { get; private set; }
+        public ObservableCollection<string> observableCollection { get; private set; }
+
+        public MainWindowViewModel()
+        {
+            observableCollection = new ObservableCollection<string>();
+            reactiveCollection = new ReactiveCollection<string>();
+        }
+
+        public void AddObservableCollection()
+        {
+            Task.Run(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    this.observableCollection.Add(i.ToString());
+                }
+            });
+        }
+
+        public void AddReactiveCollection()
+        {
+            Task.Run(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    this.reactiveCollection.AddOnScheduler(i.ToString());
+                }
+            });
+        }
+
+    }
+}
